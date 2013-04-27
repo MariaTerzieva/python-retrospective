d267f371210f1a0a6020ea5ee773853497e5f8eb
Maria Georgieva Terzieva <mimi_t92@abv.bg>
2013-04-27 20:33:17 +0300

    Use docstrings and fix iterate and cache

--- task2/solution.py
+++ task2/solution.py
@@ -3,20 +3,23 @@ from collections import defaultdict, OrderedDict
 
 def groupby(func, seq):
     result = defaultdict(list)
+
     for element in seq:
         result[func(element)].append(element)
+
     return result
 
 
+def compose(func1, func2):
+    return lambda arg: func1(func2(arg))
+
+
 def iterate(func):
-    def repeated(n, arg):
-        for i in range(n):
-            arg = func(arg)
-        return arg
-    repeat = 0
+    iterated_function = lambda arg: arg
+
     while True:
-        yield lambda arg: repeated(repeat, arg)
-        repeat += 1
+        yield iterated_function
+        iterated_function = compose(func, iterated_function)
 
 
 def zip_with(func, *iterables):
@@ -25,15 +28,16 @@ def zip_with(func, *iterables):
 
 
 def cache(func, cache_size):
+    """Keep in cache the last `cache_size` unique calls of a `func`."""
     cache_storage = OrderedDict()
 
-    def check_and_modify_cache(arg):
-        if arg in cache_storage:
-            return cache_storage[arg]
+    def check_and_modify_cache(*args):
+        if args in cache_storage:
+            return cache_storage[args]
         else:
-            result = cache_storage[arg] = func(arg)
+            result = cache_storage[args] = func(*args)
             if len(cache_storage) > cache_size:
                 cache_storage.popitem(last=False)
             return result
 
-    return lambda arg: check_and_modify_cache(arg)
+    return check_and_modify_cache
