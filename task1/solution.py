f8e4a4d1dc0a0bf8a99fdeeea405d2e964a2e80d
Maria Georgieva Terzieva <mimi_t92@abv.bg>
2013-04-27 17:03:42 +0300

    Use constants and simplify

--- task1/solution.py
+++ task1/solution.py
@@ -1,31 +1,17 @@
+SIGNS = [{20: ('Козирог', 'Водолей')},
+         {19: ('Водолей', 'Риби')},
+         {21: ('Риби', 'Овен')},
+         {21: ('Овен', 'Телец')},
+         {21: ('Телец', 'Близнаци')},
+         {21: ('Близнаци', 'Рак')},
+         {22: ('Рак', 'Лъв')},
+         {23: ('Лъв', 'Дева')},
+         {23: ('Дева', 'Везни')},
+         {23: ('Везни', 'Скорпион')},
+         {22: ('Скорпион', 'Стрелец')},
+         {22: ('Стрелец', 'Козирог')}]
+
+
 def what_is_my_sign(day, month):
-    signs = {1: ('Козирог', 'Водолей'),
-             2: ('Водолей', 'Риби'),
-             3: ('Риби', 'Овен'),
-             4: ('Овен', 'Телец'),
-             5: ('Телец', 'Близнаци'),
-             6: ('Близнаци', 'Рак'),
-             7: ('Рак', 'Лъв'),
-             8: ('Лъв', 'Дева'),
-             9: ('Дева', 'Везни'),
-             10: ('Везни', 'Скорпион'),
-             11: ('Скорпион', 'Стрелец'),
-             12: ('Стрелец', 'Козирог')}
-
-    def what_to_return(limit):
-        if day < limit:
-            return sign_tuple[0]
-        else:
-            return sign_tuple[1]
-
-    for month_number, sign_tuple in signs.items():
-        if month_number == month and month_number in [3, 4, 5, 6]:
-            return what_to_return(21)
-        elif month_number == month and month_number in [7, 11, 12]:
-            return what_to_return(22)
-        elif month_number == month and month_number in [8, 9, 10]:
-            return what_to_return(23)
-        elif month_number == month == 1:
-            return what_to_return(20)
-        elif month_number == month == 2:
-            return what_to_return(19)
+    for key, signs_tuple in SIGNS[month - 1].items():
+        return signs_tuple[day >= key]
